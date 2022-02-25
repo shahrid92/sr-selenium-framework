5f96842f9eea217e4cd92b57e4be608d3c528bfd
shahrid92 <repo.shahridzuan.aws@gmail.com>
2022-02-26 00:49:08 +0800

    wip - cucumber learning

---
+++ Selenium-Shopee/src/test/java/cucumber/stepdefinition/demo_test.java
@@ -0,0 +1,24 @@
+package cucumber.stepdefinition;
+
+import io.cucumber.java.en.*;
+
+public class demo_test {
+
+    @Given("^this is given step$")
+    public void this_is_given_step() {
+        System.out.println("Test1");
+    }
+    @When("this is when step")
+    public void this_is_when_step() {
+        System.out.println("Test2");
+    }
+    @And("this is and step")
+    public void this_is_and_step() {
+        System.out.println("Test3");
+    }
+    @Then("this is then step")
+    public void this_is_then_step() {
+        System.out.println("Test4");
+    }
+
+}
