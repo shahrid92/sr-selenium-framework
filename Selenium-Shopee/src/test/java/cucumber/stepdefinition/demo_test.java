70b843b83fad9ad49a288b443ea9339158826989
shahrid92 <repo.shahridzuan.aws@gmail.com>
2022-03-08 00:33:49 +0800

    wip

--- Selenium-Shopee/src/test/java/cucumber/stepdefinition/demo_test.java
+++ Selenium-Shopee/src/test/java/cucumber/stepdefinition/demo_test.java
@@ -3,11 +3,11 @@ package cucumber.stepdefinition;
 import BaseClass.TestBase;
 import io.cucumber.java.en.*;
 
-public class demo_test {
+public class demo_test extends TestBase {
 
     @Given("^this is given step$")
     public void this_is_given_step() {
-        System.out.println("Test1");
+        this.mainDriver("chrome",null,"demosqa.properties");
     }
     @When("this is when step")
     public void this_is_when_step() {
