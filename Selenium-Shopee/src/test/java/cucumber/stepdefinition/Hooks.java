0dd6788e1f1268fde1d7e500aff7a07bab4c5f7b
shahrid92 <repo.shahridzuan.aws@gmail.com>
2022-03-19 00:17:53 +0800

    wip - Adding Hooks

---
+++ Selenium-Shopee/src/test/java/cucumber/stepdefinition/Hooks.java
@@ -0,0 +1,26 @@
+package cucumber.stepdefinition;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class Hooks {
+
+    @Before(order=1)
+    public void beforeScenario(){
+        System.out.println("This will run before the every Scenario");
+    }
+    @Before(order=0)
+    public void beforeScenarioStart(){
+        System.out.println("-----------------Start of Scenario-----------------");
+    }
+
+
+    @After(order=0)
+    public void afterScenarioFinish(){
+        System.out.println("-----------------End of Scenario-----------------");
+    }
+    @After(order=1)
+    public void afterScenario(){
+        System.out.println("This will run after the every Scenario");
+    }
+}
